87b3a8791513707f2f23fc278147ab6382c1403e
Richard Rodger <richard@ricebridge.com>
2024-04-11 00:46:06 +0100

    fix-doc

--- apikey-doc.js
+++ apikey-doc.js
@@ -2,29 +2,31 @@ module.exports = function (seneca, util) {
   var Joi = util.Joi
 
   return {
-    generate_key: {
-      desc: 'Generate a new API key.',
-      reply_desc: {
-        ok: '`true` if successful',
-        key: 'key string',
+    messages: {
+      generate_key: {
+        desc: 'Generate a new API key.',
+        reply_desc: {
+          ok: '`true` if successful',
+          key: 'key string',
+        },
+        validate: {
+          owner: Joi.string().required(),
+          scope: Joi.string().default('default'),
+        },
       },
-      validate: {
-        owner: Joi.string().required(),
-        scope: Joi.string().default('default'),
-      },
-    },
 
-    verify_key: {
-      desc: 'Verify an API key.',
-      reply_desc: {
-        ok: '`true` if verified',
-        why: 'explanation code',
+      verify_key: {
+        desc: 'Verify an API key.',
+        reply_desc: {
+          ok: '`true` if verified',
+          why: 'explanation code',
+        },
+        validate: {
+          owner: Joi.string().required(),
+          scope: Joi.string().default('default'),
+          key: Joi.string().required(),
+        },
       },
-      validate: {
-        owner: Joi.string().required(),
-        scope: Joi.string().default('default'),
-        key: Joi.string().required(),
-      },
-    },
+    }
   }
 }
